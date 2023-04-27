a60c423b6d9a57f6cffa1d7b81b9a299cc684ec9
Isha Bam <79683608+Ishabam@users.noreply.github.com>
2023-04-27 09:30:29 +0530

    Pricing page and template overview page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TemplateOverview from './components/TemplateOverview';
+import PricingPage from './components/PricingPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='bg-red-400'>hello world</h1>
+    <div>
+       <Router>
+        <Routes>
+          <Route path="/templateOverview" element={<TemplateOverview />} />
+          <Route path="/pricingPage" element={<PricingPage />} />
+        </Routes>
+        </Router>
     </div>
-  );
+    );
 }
 
 export default App;
