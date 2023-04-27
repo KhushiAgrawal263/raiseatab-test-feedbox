3a9e3b7fc87da07bbc0c22b4331b0c813ff360a4
Aditya Pandey <adityapandey272002@gmail.com>
2023-04-27 12:50:04 +0530

    log in and register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
 import Home_Page from './Components/Home_Page/Home_Page';
-
+import {BrowserRouter, Routes,Route } from 'react-router-dom';
+import Login from './Components/Login_Page/Login';
+import Register from './Components/Register_Page/Register';
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
       <NavBar/>
-      <Home_Page/>
+      <Routes>
+        <Route path="/" element={<Home_Page/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/register" element={<Register/>}></Route>
+      </Routes>
+      
+
+      </BrowserRouter>
     </div>
   );
 }
