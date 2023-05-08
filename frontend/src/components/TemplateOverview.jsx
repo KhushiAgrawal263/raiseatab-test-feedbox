5655f1446f5ee026f9b796c3c86acae10cc6f0b5
Isha Bam <79683608+Ishabam@users.noreply.github.com>
2023-05-08 11:39:47 +0530

    unique id generated

--- frontend/src/components/TemplateOverview.jsx
+++ frontend/src/components/TemplateOverview.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import template from "./assests/template.png";
 import { Link } from "react-router-dom";
 
 const TemplateOverview = () => {
+  const [uniqueId, setUniqueId] = useState();
+
+  const generateUniqueid = () => {
+    let c3 = Math.floor(1000 + Math.random() * 9000);
+
+    let result = '';
+    for (let i = 0; i < 1; i++) {
+      result += c3;
+    }
+
+    console.log('unique id',result);
+    setUniqueId(result);
+  }
+
   return (
     <div className="  flex flex-wrap dark:bg-gray-900 ">
       <div className="align-center text-center pt-6 w-full font-bold text-[40px] dark:text-white">Templates</div>
       <div class="flex items-center justify-center flex-wrap pt-5">
 
         <card class="m-3 relative h-[20rem] w-[25rem] rounded-lg hover:scale-105 transition-all shadow-md">
-          <Link to='/invoicePage' state={{name:"mentoring"}}>
+          <Link to='/invoicePage' state={{name:"mentoring"}} onClick={generateUniqueid}>
           <img alt="" src={template} class="object-cover w-full h-full rounded-lg" />
           <div class="absolute w-full h-full bottom-0 dark:bg-gray-800  opacity-90 rounded-lg flex flex-col items-center justify-center text-center">
             <div className="text-white font-bold text-[30px] w-[50%]">Mentoring Invoice</div>
@@ -18,7 +32,7 @@ const TemplateOverview = () => {
         </card>
 
         <card class="m-3 relative h-[20rem] w-[25rem] rounded-lg hover:scale-105 transition-all shadow-md">
-          <Link to='/invoicePage' state={{name:"generic"}}>
+          <Link to='/invoicePage' state={{name:"generic"}} onClick={generateUniqueid}>
           <img alt="" src={template} class="object-cover w-full h-full rounded-lg" />
           <div class="absolute w-full h-full bottom-0 dark:bg-gray-800  opacity-90 rounded-lg flex flex-col items-center justify-center text-center">
             <div className="text-white font-bold text-[30px] w-[50%]">Generic Consulting Invoice</div>
@@ -27,7 +41,7 @@ const TemplateOverview = () => {
         </card>
 
         <card class="m-6 relative  h-[20rem] w-[25rem] rounded-lg hover:scale-105 transition-all  text-gray-900 bg-white  border shadow dark:border-gray-600  dark:bg-gray-800 dark:text-white">
-          <Link to='/invoicePage' state={{name:"reference"}}>
+          <Link to='/invoicePage' state={{name:"reference"}} onClick={generateUniqueid}>
           <img alt="" src={template} class="object-cover w-full h-full rounded-lg" />
           <div class="absolute w-full h-full bottom-0 dark:bg-gray-800  opacity-90 rounded-lg flex flex-col items-center justify-center text-center">
             <div className="text-white font-bold text-[30px] w-[50%]">Reference Introduction</div>
@@ -36,7 +50,7 @@ const TemplateOverview = () => {
         </card>
 
         <card class="m-6 relative  h-[20rem] w-[25rem] rounded-lg hover:scale-105 transition-all  text-gray-900 bg-white  border shadow dark:border-gray-600  dark:bg-gray-800 dark:text-white">
-          <Link to='/invoicePage' state={{name:"business"}}>
+          <Link to='/invoicePage' state={{name:"business"}} onClick={generateUniqueid}>
           <img alt="" src={template} class="object-cover w-full h-full rounded-lg" />
           <div class="absolute w-full h-full bottom-0 dark:bg-gray-800  opacity-90 rounded-lg flex flex-col items-center justify-center text-center">
             <div className="text-white font-bold text-[30px] w-[50%]">Business Development Fee </div>
@@ -45,7 +59,7 @@ const TemplateOverview = () => {
         </card>
 
         <card class="m-6 relative  h-[20rem] w-[25rem] rounded-lg hover:scale-105 transition-all  text-gray-900 bg-white  border shadow dark:border-gray-600  dark:bg-gray-800 dark:text-white">
-          <Link to='/invoicePage' state={{name:"technical"}}>
+          <Link to='/invoicePage' state={{name:"technical"}} onClick={generateUniqueid}>
           <img alt="" src={template} class="object-cover w-full h-full rounded-lg" />
           <div class="absolute w-full h-full bottom-0 dark:bg-gray-800  opacity-90 rounded-lg flex flex-col items-center justify-center text-center">
             <div className="text-white font-bold text-[30px] w-[50%]">Technical Consultation</div>
@@ -54,14 +68,14 @@ const TemplateOverview = () => {
         </card>
 
         <card class="m-6 relative  h-[20rem] w-[25rem] rounded-lg hover:scale-105 transition-all  text-gray-900 bg-white  border shadow dark:border-gray-600  dark:bg-gray-800 dark:text-white">
-          <Link to='/invoicePage'state={{name:"basic"}} >
+          <Link to='/invoicePage'state={{name:"basic"}} onClick={generateUniqueid}>
           <img src={template} alt="" class="object-cover w-full h-full rounded-lg" />
           <div class="absolute w-full h-full bottom-0 dark:bg-gray-800  opacity-90 rounded-lg flex flex-col items-center justify-center text-center">
             <div className="text-white font-bold text-[30px] w-[50%]">Basic Templates</div>
           </div>
           </Link>
         </card>
-
+        {/* </form> */}
       </div>
     </div>
   );
